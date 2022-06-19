3be6c13468c3c704bb0d4680abc7295d89638f77
Stefan Neufeldt <stefan.neufeldt@stud.hs-flensburg.de>
2022-06-19 14:25:46 +0200

    Added Aufgabe 3_2

---
+++ Aufgabe3/src/main/kotlin/Aufgabe3_2.kt
@@ -0,0 +1,14 @@
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.runBlocking
+
+val numbersFlow = flowOf(1, 2, 3).onEach { delay(1000) }
+val lettersFlow = flowOf("A", "B", "C").onEach { delay(2000) }
+
+fun main() {
+    runBlocking {
+
+    }
+}
